fedeceadcd4c0e19f57755b30205c163d7025803
Gilles Wittenberg <git@gilleswittenberg.com>
2011-08-08 12:18:26 +0200

    init generator

--- js/init.js
+++ js/init.js
@@ -1,1 +1,11 @@
 $('textarea').whatYouSimple();
+
+var div = $('div.textarea');
+var pre = $('pre');
+
+function display(){
+	var html = htmlentities(div.html());
+	pre.html(html);	
+}
+
+$('menu a').click(display);
